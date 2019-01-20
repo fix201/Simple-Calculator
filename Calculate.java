ced27a7cd73895b53c2897facfacbfabc3a223f6
Olufisayo Joseph Ayodele <fisayoayodele01@gmail.com>
2019-01-19 19:04:47 -0700

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,35 @@
+package cal;
+
+import java.awt.EventQueue;
+import java.awt.Frame;
+
+public class Calculate {
+
+	private MiniCalculator window = new MiniCalculator();
+	private Frame frame = new Frame();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new Calculate().getWindow().getFrame().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public MiniCalculator getWindow() {
+		return window;
+	}
+
+	public Frame getFrame() {
+		return frame;
+	}
+
+}
